58b4e716c8657c1af68f70dbca6a0af7ea68ee71
Muhammad Sibra <ilmuoneinfo@gmail.com>
2024-11-17 07:48:01 +0700

    feat: add productf

--- src/app/modules/home/services/services.constant.ts
+++ src/app/modules/home/services/services.constant.ts
@@ -2,6 +2,11 @@ export const SERVICES = [
   {
     title: 'Bendera Merah Putih (120 x 80)',
     image: 'assets/services/prod-1.webp',
-    note: 'Bahan Kahatex'
+    note: 'Bahan Peles Kahatex'
+  },
+  {
+    title: 'Bendera Merah Putih (170 x 110)',
+    image: 'assets/services/prod-2.webp',
+    note: 'Bahan Peles Kahatex'
   },
 ];
